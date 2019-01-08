c3fe332493b4860178146b7ca847511b300191c2
LanZhisheng <kevinlan@aijiatui.com>
2019-01-08 19:26:05 +0800

    feat:初始化

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import multipleCheck from './vue-multiple-check.vue';
+export default crop;
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.component('multipleCheck', multipleCheck);
+}
